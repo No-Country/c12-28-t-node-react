6cfb4f2532d069349fb02dfbd8d6c7e56cade178
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-07-21 22:24:22 -0600

    Refactor Coding

--- client/src/components/RegisterCompleted.tsx
+++ client/src/components/RegisterCompleted.tsx
@@ -1,26 +1,12 @@
-import logo from "../../assets/Checkmark.png"
+import logo from "../assets/Checkmark.png"
 
-const RegisterCompleted = () => {
-
-    const data = [
-
-        {
-            imagen:logo, title: "Felicidades te has registrado satisfactoriamente, por favor no cierres"
-        }
-    ]
-
+const RegisterCompleted = (prop:any) => {
+    console.log("ejecutando")
     return (
-        <>
-
-        {
-            data.map((item,index) => (
-                <div key={index} className="  flex flex-col justify-center items-center rounded-2xl ml-[148px] mr-[213px] bg-[#CBC9C9]">
-                    <p className="text-[#0D1F13] text-[30px]">{item.title}</p>
-                    <img src={item.imagen} alt="" />
-                </div>
-            ))
-        }
-        </>
+        <div className="  flex flex-col justify-center items-center rounded-2xl ml-[148px] mr-[213px] bg-[#CBC9C9]">
+            <p className="text-[#0D1F13] text-[30px]">{prop.message}</p>
+            <img src={logo} alt="" />
+        </div>
     )
 }
 export default RegisterCompleted
